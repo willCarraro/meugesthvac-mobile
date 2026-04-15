4e95f016eaa07c6503e229709c000595e6e00de0
Will Carraro <uiltamarcarraro1@gmail.com>
2026-04-15 16:09:28 -0300

    Update customer.ts

--- src/features/customers/types/customer.ts
+++ src/features/customers/types/customer.ts
@@ -1,1 +1,10 @@
-
+export type Customer = {
+  id: string
+  name: string
+  document?: string
+  phone?: string
+  email?: string
+  address?: string
+  company_id: string
+  created_at?: string
+}
